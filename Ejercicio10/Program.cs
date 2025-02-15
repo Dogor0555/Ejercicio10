71aa53ddcb69018e21f8f7c445568a1afaa9a423
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:06:02 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.Write("Ingrese un número entre 1 y 7 para representar los días de la semana: ");
+int dia = int.Parse(Console.ReadLine());
+
+switch (dia)
+{
+    case 1:
+        Console.WriteLine("Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Martes");
+        break;
+    case 3:
+        Console.WriteLine("Miércoles");
+        break;
+    case 4:
+        Console.WriteLine("Jueves");
+        break;
+    case 5:
+        Console.WriteLine("Viernes");
+        break;
+    case 6:
+        Console.WriteLine("Sábado");
+        break;
+    case 7:
+        Console.WriteLine("Domingo");
+        break;
+    default:
+        Console.WriteLine("Número inválido. Ingrese un valor entre 1 y 7.");
+        break;
+}
